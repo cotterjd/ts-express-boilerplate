6b784114bccbcacb089061ba90c13e787aee793d
Jordan Cotter <jrdn.cttr@gmail.com>
2021-09-26 13:21:46 -0500

    feat: add nodemon

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get(`/votes`, (req: Request, res: Response) => {
-  return res.send(`hello`);
+app.get(`/`, (req: Request, res: Response) => {
+  return res.send(`hello!`);
 });
 
 const port = 3000;
